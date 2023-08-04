f56daeb586c54124ffb7a442b07eb3c4c0df3477
Hyrone Shutter <hyrone.shutter@gmail.com>
2023-08-04 09:58:53 +0200

    ajout du code slider next est previous

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,71 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// declarations variables
+let arrowLeft = document.querySelector('.arrow_left');
+
+let arrowRight = document.querySelector('.arrow_right');
+
+let bannerImg = document.querySelector('.banner-img');
+
+let bannerTitle = document.querySelector('.banner-title');
+
+let dots = document.querySelector('.dots');
+
+let currentSlide = 0;
+
+
+// ecouteur d'evenement des fleches droite et gauche
+arrowLeft.addEventListener("click", function() {
+	previousSlide()
+});
+
+arrowRight.addEventListener("click", function() {
+	nextSlide()
+});
+
+
+dots = slides.length;
+console.log(dots);
+
+
+function nextSlide(){
+currentSlide++;
+	if(currentSlide === slides.length){
+		currentSlide = 0;
+	}
+	bannerImg.src = slides[currentSlide].image;
+	bannerTitle.innerHTML = slides[currentSlide].tagLine;
+
+	console.log(bannerTitle, bannerImg);
+	console.log('click fleche droite')
+};
+  
+function previousSlide(){
+	currentSlide--;
+	if(currentSlide < 0){
+		currentSlide = slides.length - 1;
+	}
+	bannerImg.src = slides[currentSlide].image;
+	bannerTitle.innerHTML = slides[currentSlide].tagLine;
+
+	console.log(bannerTitle, bannerImg);
+	console.log('click fleche gauche')
+};
+
